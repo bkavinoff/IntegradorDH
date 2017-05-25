934de99a0487944e8f8f345d449c009ba5a08ec7
bkavinoff@gmail.com <bkavinoff@gmail.com>
2017-05-25 20:26:50 -0300

    Primera version del Home

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>TP Integrador</title>
+  </head>
+  <body>
+    <div class="container">
+      <!--//////////////<MENU SUPERIOR>///////////////-->
+      <nav class="navbar"><!--   navbar-inverse  -->
+          <div class="container">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="#">Integrador DH</a>
+            </div>
+            <div class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Inicio</a></li>
+                <li><a href="#faq">F.A.Q.</a></li>
+                <li><a href="#registrar">Registrarse</a></li>
+                <li><a href="#loguear">Loguearse</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      <!--//////////////</MENU SUPERIOR>///////////////-->
+
+      <!--//////////////<CONTENIDO>///////////////-->
+      <div class="main-header">
+        <div class="jumbotron text-center">
+          <h1>Bienvenidos</h1>
+          <p>Acá va a ir una pequeña descripcion de la página...</p>
+        </div>
+      </div>
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-sm-4 fondo-azul producto">
+            <img src="images/Iphone.png" alt="Iphone" class="imgProducto">
+				    <h2 class="tituloProducto">Producto</h2>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+				    <button type="button" class="btn btn-success vermas">Ver Mas</button>
+          </div>
+          <div class="col-sm-4 fondo-rojo producto">
+            <img src="images/SmartphonesAndroid.png" alt="SmartphonesAndroid" class="imgProducto">
+				    <h2 class="tituloProducto">Producto</h2>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+				    <button type="button" class="btn btn-success vermas">Ver Mas</button>
+          </div>
+          <div class="col-sm-4 fondo-verde producto">
+            <img src="images/Tablet.png" alt="Tablet" class="imgProducto">
+				    <h2 class="tituloProducto">Producto</h2>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+				    <button type="button" class="btn btn-success vermas">Ver Mas</button>
+          </div>
+        </div>
+      </div>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-sm-4 fondo-azul producto">
+            <img src="images/PC.png" alt="PC" class="imgProducto">
+				    <h2 class="tituloProducto">Producto</h2>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+				    <button type="button" class="btn btn-success vermas">Ver Mas</button>
+          </div>
+          <div class="col-sm-4 fondo-rojo producto">
+            <img src="images/Notebook.png" alt="Notebook" class="imgProducto">
+				    <h2 class="tituloProducto">Producto</h2>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+				    <button type="button" class="btn btn-success vermas">Ver Mas</button>
+          </div>
+          <div class="col-sm-4 fondo-verde producto">
+            <img src="images/Presupuestos.png" alt="Presupuestos" class="imgProducto">
+				    <h2 class="tituloProducto">Producto</h2>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+				    <button type="button" class="btn btn-success vermas">Ver Mas</button>
+          </div>
+        </div>
+      </div>
+      <!--//////////////<CONTENIDO>///////////////-->
+
+      <!--//////////////<FOOTER>///////////////-->
+      <div class="container-fluid footer">
+            <footer>
+              <p class="text-center">&copy; Copyright 2017  Andres Barrios - Brian Kavinoff - Sebastian Portela</p>
+            </footer>
+      </div>
+    </div>
+    <!--//////////////</FOOTER>///////////////-->
+  </body>
+</html>
