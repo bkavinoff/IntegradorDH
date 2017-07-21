928817c60850855f3007b1831f30c1d3be9ba099
andresbarrios16@hotmail.com <andresbarrios16@hotmail.com>
2017-07-20 21:23:33 -0300

    Tabulaciones

--- faq.php
+++ faq.php
@@ -1,81 +1,72 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Preguntas frecuentes</title>
-    <?php require "cabecera.php";?>
-  </head>
-  <body>
+    <head>
+        <title>Preguntas frecuentes</title>
+        <?php require "cabecera.php";?>
+    </head>
+    <body>
 
-<div class="container-fluid">
-      <!--//////////////<MENU SUPERIOR>///////////////-->
-      <?php require "navbar.php";?>
-      <!--//////////////</MENU SUPERIOR>///////////////-->
+        <div class="container-fluid">
+        <!--//////////////<MENU SUPERIOR>///////////////-->
+            <?php require "navbar.php";?>
+        <!--//////////////</MENU SUPERIOR>///////////////-->
 
-      <!--//////////////<CABECERA>///////////////-->
-      <div class="container">
-      <div class="panel panel-success">
-          <div class="panel-heading">
-              Preguntas Frecuentes
-          </div>
+        <!--//////////////<CABECERA>///////////////-->
+        <div class="container">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    Preguntas Frecuentes
+                </div>
 
-      </div>
+            </div>
+            <!--//////////////<CABECERA>///////////////-->
 
-      <!--//////////////<CABECERA>///////////////-->
+            <!--//////////////<CONTENIDO>///////////////-->
+                <div class="container-fluid producto faq-container">
+                    <div class="row">
+                        <div class="faq-container display-answer " >
+                            <h2>¿Este servicio es para todo el país?</h2>
+                                <p>Podés comprar desde cualquier lugar del país pero, (por el momento) las entregas se realizan solo en CAP FED Y GBA.</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="faq-container display-answer " >
+                            <h2>¿Cómo me registro?</h2>
+                                <p>Podrás crear tu cuenta en el siguiente link: <a href="registro.php">REGISTRARME</a> donde debés completar todos los datos solicitados.</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class=" faq-container display-answer">
+                            <h2>¿Cómo recupero mi clave si la olvidé?</h2>
+                                <p>Si olvidaste tu clave, ingresá al siguiente link: <a href="#OlvidasteCotnraseña">RECUPERAR CONTRASEÑA</a> </p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class=" faq-container display-answer">
+                            <h2>¿Cómo realizo mi compra?</h2>
+                                <p>Ingresá a nuestra <a href="index.php">WEB</a> y registrate o comprá como invitado. Te recomendamos registrarte ya que accedés a Múltiples beneficios.
+                                    Luego seleccioná la opción <a href="index.php">INICIO</a> para poder navegar en nuestro catálogo de productos y realizar tus compras de manera rápida, fácil y simple.</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class=" faq-container display-answer">
+                            <h2>¿Tenés inconvenientes al abonar con tu tarjeta de crédito?</h2>
+                            <p>-Fondos insuficientes en la tarjeta de crédito (si tenés alguna duda, comunícate con la entidad bancaria a la brevedad).<br>
+                                -Error en el sistema: Comunicate con nosotros a nuestro 0800-000-0000 opción 2. <br>
+                                -Llamar al emisor: cuando figure este mensaje, deberás comunicarte con el emisor de tu tarjeta. <br>
+                            <br>
+                                Por cualquier consulta, siempre podés llamar a nuestro 0800-000-0000 opción 2.</p>
 
+                        </div>
+                    </div>
 
-      <!--//////////////<CONTENIDO>///////////////-->
-    <div class="container-fluid producto faq-container">
-
-      <div class="row">
-        <div class="faq-container display-answer " >
-          <h2>¿Este servicio es para todo el país?</h2>
-          <p>Podés comprar desde cualquier lugar del país pero, (por el momento) las entregas se realizan solo en CAP FED Y GBA.</p>
+                </div>
         </div>
-      </div>
+        <!--//////////////<CONTENIDO>///////////////-->
 
-      <div class="row">
-        <div class="faq-container display-answer " >
-          <h2>¿Cómo me registro?</h2>
-          <p>Podrás crear tu cuenta en el siguiente link: <a href="registro.php">REGISTRARME</a> donde debés completar todos los datos solicitados.</p>
-        </div>
-      </div>
-      <div class="row">
-        <div class=" faq-container display-answer">
-        <h2>¿Cómo recupero mi clave si la olvidé?</h2>
-        <p>Si olvidaste tu clave, ingresá al siguiente link: <a href="#OlvidasteCotnraseña">RECUPERAR CONTRASEÑA</a> </p>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class=" faq-container display-answer">
-        <h2>¿Cómo realizo mi compra?</h2>
-        <p>Ingresá a nuestra <a href="index.php">WEB</a> y registrate o comprá como invitado. Te recomendamos registrarte ya que accedés a Múltiples beneficios.
-          Luego seleccioná la opción <a href="index.php">INICIO</a> para poder navegar en nuestro catálogo de productos y realizar tus compras de manera rápida, fácil y simple.</p>
-        </div>
-      </div>
-      <div class="row">
-        <div class=" faq-container display-answer">
-        <h2>¿Tenés inconvenientes al abonar con tu tarjeta de crédito?</h2>
-          <p>-Fondos insuficientes en la tarjeta de crédito (si tenés alguna duda, comunícate con la entidad bancaria a la brevedad).<br>
-            -Error en el sistema: Comunicate con nosotros a nuestro 0800-000-0000 opción 2. <br>
-            -Llamar al emisor: cuando figure este mensaje, deberás comunicarte con el emisor de tu tarjeta. <br>
-            <br>
-            Por cualquier consulta, siempre podés llamar a nuestro 0800-000-0000 opción 2.</p>
-
-        </div>
-      </div>
-
-    </div>
-
-    <!--//////////////<CONTENIDO>///////////////-->
-
-</div>
     <!--//////////////</FOOTER>///////////////-->
         <?php require "footer.php";?>
-
       <!--//////////////</FOOTER>///////////////-->
-
-</div>
-
-  </body>
+        </div>
+    </body>
 </html>
--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div class="container-fluid footer">
       <footer>
-        <p class="text-center">&copy; Copyright 2017  Andres Barrios - Brian Kavinoff - Javier Portela</p>
+          <p class="text-center">&copy; Copyright 2017  Andres Barrios - Brian Kavinoff - Javier Portela</p>
       </footer>
 </div>
